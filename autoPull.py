b4b47dd3efe671f67d12e12b60134204b09291b6
andycall <dongtiangche@gmail.com>
2014-12-25 01:26:11 +0800

    autoPull complete

--- autoPull.py
+++ autoPull.py
@@ -2,9 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'andycall'
 
-from multiprocessing import Process, Pool
-import subprocess
-import os, time, random, gittle, logging
+import subprocess, os, logging
 
 
 def checkIfGit(str):
@@ -13,13 +11,11 @@ def checkIfGit(str):
     else:
         return False
 
-
 paths = []
 
 def find(path):
     if not os.path.exists(path):
         return
-
 
     dir_list = os.listdir(path)
 
@@ -30,51 +26,23 @@ def find(path):
         else:
             if os.path.isdir(new_path):
                 find(new_path)
-            # else:
-            #     with open(new_path, 'r') as f:
-            #         if str in f.read():
-            #             print new_path
-
-
-# def Pull(path, index):
-#     print 'Running {index} of {total}'.format(index=index, total=path_length)
-#
-#     # log = os.popen('git pull ' + path).read()
-#     name = path.split('/')[-2]
-#
-#     print name
-#
-#     if not os.path.exists(now_path + 'log'):
-#         os.mkdir(now_path + 'log')
-#
-#     with open(os.path.join(now_path, 'log/{index}.log'.format(index=index)), 'wb') as f:
-#         f.write(log)
-
 
 def RunPull(paths):
-    # p = Pool(4)
     now_path = os.path.abspath('.')
-
 
     for i in paths:
         name = i.split('/')[-1]
         try:
-            output = subprocess.check_output(['git', 'pull', i])
+            os.chdir(i)
+            now_branch = subprocess.check_output(['git', 'branch']).split(' ')[-1].strip('\n')
+            now_origin = subprocess.check_output(['git', 'remote']).strip('\n')
+            output = subprocess.check_output(['git', 'pull', now_origin, now_branch])
             with open(now_path + '/log/{name}.log'.format(name=name), 'wb') as f:
                 f.write("---------- " + name + '------------\n' + output + '\n')
         except subprocess.CalledProcessError, e:
             logging.exception(e)
             with open(now_path + '/log/error.log', 'wb') as f:
-                f.write("Failed :" + name + "\n")
-
-    # for i in range(len(paths)):
-    #     p.apply_async(Pull, args=(paths[i], i))
-    #
-    # print('Wait for all subProcess done')
-    # p.close()
-    # p.join()
-    # print("All work Done")
-
+                f.write("Failed :" + name + '\n' + e + '\n')
 
 if __name__ == "__main__":
 
