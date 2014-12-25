03f02564b230b4bd72c7bfc4bbc933cbf35e99f1
andycall <dongtiangche@gmail.com>
2014-12-25 10:37:19 +0800

    autoPull complete

--- autoPull.py
+++ autoPull.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'andycall'
 
-import subprocess, os, logging
-
+import subprocess, os, logging, time
 
 def checkIfGit(str):
     if '.git' in str and os.path.isdir(str):
@@ -29,20 +28,28 @@ def find(path):
 
 def RunPull(paths):
     now_path = os.path.abspath('.')
+    log_path = os.path.join(now_path, 'log')
+    now_time = time.strftime('%Y-%m-%d %H:%m',time.localtime(time.time()))
+
+    if not os.path.exists(log_path):
+        os.mkdir(log_path)
+
+    with open(os.path.join(log_path, 'error.log'), 'a') as f:
+        f.write('--------------- ' + now_time + ' ----------------\n')
 
     for i in paths:
         name = i.split('/')[-1]
         try:
             os.chdir(i)
-            now_branch = subprocess.check_output(['git', 'branch']).split(' ')[-1].strip('\n')
-            now_origin = subprocess.check_output(['git', 'remote']).strip('\n')
+            now_branch = subprocess.check_output(['git', 'branch']).split(' ')[-1].strip('\n') or 'master'
+            now_origin = subprocess.check_output(['git', 'remote']).strip('\n') or 'origin'
             output = subprocess.check_output(['git', 'pull', now_origin, now_branch])
-            with open(now_path + '/log/{name}.log'.format(name=name), 'wb') as f:
-                f.write("---------- " + name + '------------\n' + output + '\n')
+            with open(os.path.join(log_path ,'{name}.log'.format(name=name)), 'a') as f:
+                f.write("---------- " + now_time + ' ' + name + ' ------------\n' + output + '\n')
         except subprocess.CalledProcessError, e:
             logging.exception(e)
-            with open(now_path + '/log/error.log', 'wb') as f:
-                f.write("Failed :" + name + '\n' + e + '\n')
+            with open(os.path.join(now_path,'log/error.log'), 'a') as f:
+                f.write("Failed :" +  name + '\n')
 
 if __name__ == "__main__":
 
